ba4cbc81e5871cec1b1abe6309f937de0f3acab4
Youssra1999 <100140863+Youssra1999@users.noreply.github.com>
2024-04-22 12:25:07 +0100

    full pegasos

--- project1.py
+++ project1.py
@@ -106,24 +106,6 @@ def average_perceptron(feature_matrix, labels, T):
     return (theta_final, theta_0_final)
 
 def pegasos_single_step_update(
-        feature_vector,
-        label,
-        L,
-        eta,
-        current_theta,
-        current_theta_0):
-   
-    if label * (np.sum(feature_vector * current_theta) + current_theta_0) <= 1 :
-        new_theta = (1 - eta*L)*current_theta  +  eta*label*feature_vector
-        new_theta_0 = current_theta_0 + eta*label 
-    else:
-        new_theta = (1 - eta*L)*current_theta
-        new_theta_0 = current_theta_0
-    return (new_theta, new_theta_0)
-
-
-
-def pegasos_single_step_update(
     feature_vector,  # Input feature vector for the data point.
     label,  # Label of the data point.
     L,  # Regularization parameter.
@@ -144,6 +126,20 @@ def pegasos_single_step_update(
     # Return the updated weight vector and bias term.
     return (new_theta, new_theta_0)
 
+def pegasos(feature_matrix, labels, T, L):
+    """
+
+    (nsamples, nfeatures) = feature_matrix.shape
+    theta = np.zeros(nfeatures)
+    theta_0 = 0
+    count = 0
+    for t in range(T):
+        for i in get_order(nsamples):
+            count += 1
+            eta = 1.0 / np.sqrt(count)
+            (theta, theta_0) = pegasos_single_step_update(
+                feature_matrix[i], labels[i], L, eta, theta, theta_0)
+    return (theta, theta_0)
 
 # Part II
 
